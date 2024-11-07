70275b53e98041ed6ff107bb8f970f1db55a53f2
Krishna1423 <37060902+Krishna1423@users.noreply.github.com>
2024-11-06 19:50:47 -0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 //authorization key for calling coinmarketcap apis
-const COINMARKETCAP_API_KEY = "f31bdda3-0270-4955-92d1-b3df53aab420";
+const COINMARKETCAP_API_KEY = "";
 
 module.exports = { COINMARKETCAP_API_KEY };
